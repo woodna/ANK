201267f853326e1eef090386d24c20bad8290be6
Nate Wood <nathaniel.m.wood@lawrence.edu>
2020-03-11 11:45:15 -0500

    endofCommit

--- animaltracker/src/main/java/com/ank/App.java
+++ animaltracker/src/main/java/com/ank/App.java
@@ -1,13 +1,25 @@
 package com.ank;
 
+import com.ank.controller.Controller;
+
+import org.springframework.beans.factory.config.BeanReference;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 /**
- * Hello world!
+ *
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext ctx = 
+           new ClassPathXmlApplicationContext("applicationContext.xml");
+        Controller controller = 
+           ctx.getBean("animalController", Controller.class);
+        controller.run();
     }
 }
+
