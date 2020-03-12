f54ffb69148559acb3aaba5cf0db89b08175255b
agneshongs <agneshongs@users.noreply.github.com>
2020-03-12 10:17:10 -0500

    adding location to view

--- animaltracker/src/main/java/com/ank/ui/view.java
+++ animaltracker/src/main/java/com/ank/ui/view.java
@@ -36,11 +36,14 @@ public class view {
         int population = io.readInt("Please enter the number of animals in the herd");
         int health = io.readInt("Please enter the number of healthy animals");
         BigDecimal singleSellPrice = io.readPosDecimal("Please enter the sale price of a single animal in the herd");
+        int locationX = io.readInt("Please enter a number");
+        int locationY = io.readInt("Please enter another number");
         Herd currentHerd = new Herd(name);
         currentHerd.setName(name);
         currentHerd.setHealth(health);
         currentHerd.setPopulation(population);
         currentHerd.setSellPrice(singleSellPrice);
+        //currentHerd.setLocation(locationX, locationY);
         return currentHerd;
     }
     public void displayAddSuccessBanner() {
